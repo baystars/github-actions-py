6adf936fb0ea76ebf66987281035103a08374cb8
higebobo <higebobo@gmail.com>
2021-04-15 08:44:24 +0900

    set gh-pages

--- app.py
+++ app.py
@@ -11,9 +11,12 @@ PUB_DIR = './public'
 
 def main():
     os.makedirs(PUB_DIR, exist_ok=True)
-    print(contentful.__version__)
-    print(html2markdown.__dict__)
-    print(RichTextRenderer.__dict__)
+    contentful.__version__
+    html2markdown.__dict__
+    RichTextRenderer.__dict__
+
+    with open(os.path.join(PUB_DIR, 'index.html'), mode='w') as f:
+        f.write('<h1>hello</h1>')
 
 
 if __name__ == "__main__":
