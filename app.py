95f98f4de963e5229147416c1f5079c60ed7ecbf
higebobo <higebobo@gmail.com>
2021-04-15 08:38:28 +0900

    actions version 0.2

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 # -*- mode: python -*- -*- coding: utf-8 -*-
 import os
-import time
 
 import contentful
 import html2markdown
@@ -15,7 +14,6 @@ def main():
     print(contentful.__version__)
     print(html2markdown.__dict__)
     print(RichTextRenderer.__dict__)
-    print(time.localtime())
 
 
 if __name__ == "__main__":
