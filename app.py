5afffd27c37011df6fe50abae73851ee51e12755
higebobo <higebobo@gmail.com>
2021-04-15 08:31:58 +0900

    modified

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 # -*- mode: python -*- -*- coding: utf-8 -*-
+import os
 import time
+
+import contentful
+import html2markdown
+from rich_text_renderer import RichTextRenderer
+
+
+PUB_DIR = './public'
 
 
 def main():
+    os.makedirs(PUB_DIR, exist_ok=True)
+    print(contentful.__version__)
+    print(html2markdown.__dict__)
+    print(RichTextRenderer.__dict__)
     print(time.localtime())
 
 
