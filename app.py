46216f03c81ade1d6af74068e0c9d078cbc2a365
higebobo <higebobo@gmail.com>
2021-04-15 09:15:18 +0900

    modified

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def main():
     RichTextRenderer.__dict__
 
     with open(os.path.join(PUB_DIR, 'index.html'), mode='w') as f:
-        f.write('<h1>hello</h1>')
+        f.write('<h1>hello</h1><p>Welcome Github Actions</p>')
 
 
 if __name__ == "__main__":
